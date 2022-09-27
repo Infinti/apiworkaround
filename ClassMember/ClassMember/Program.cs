f8fcdc9300f0ea42f6e95065eb975c6722826310
Infinti <79116441+Infinti@users.noreply.github.com>
2022-09-27 19:06:36 +0300

    Add files via upload

---
+++ ClassMember/ClassMember/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassMember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            okul o = new okul();
+
+            
+        }
+    }
+    class okul
+    {
+        #region property dersi
+        //string cadi;
+        //    public string Cadi
+        //    {
+        //        get
+        //        {
+        //            return cadi;
+        //        }
+        //        set
+        //        {
+        //            cadi = value;
+        //        }
+        //    }
+        //bool bayrak;//CTRL+R+E ya da soldan bul ve seç//java get set metodu
+
+        //public bool Bayrak { get => bayrak; set => bayrak = value; }
+        //public string Cadi { get; set; } = "deneme";//ilk oluşturulurken atama yapılabilir.
+        #endregion
+
+        #region Constructor dersi
+        public int x { get; set; }
+
+        public okul()
+        {
+            x = 11;
+        }
+
+        #endregion
+    }
+}
---
+++ stajici/ArrayClass/ArrayClass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ArrayClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Sayilar = new int[5];
+            Sayilar[0] = 21;
+            Sayilar[1] = 23;
+            Sayilar[2] = 25;
+            Sayilar[3] = 26;
+            Sayilar[4] = 41;
+            #region Özellikler
+            //Console.WriteLine(Sayilar.Length);
+            //Console.WriteLine(Sayilar.Rank);
+            //Console.WriteLine(Sayilar.IsReadOnly);
+            //Console.WriteLine(Sayilar.IsFixedSize);
+            #endregion
+
+            #region Metodlar
+            //foreach (int i in Sayilar) Console.WriteLine(i);
+            //Console.WriteLine("----------------");
+            //Array.Clear(Sayilar, 0, Sayilar.Length);
+            //foreach (int i in Sayilar) Console.WriteLine(i);
+
+            //int[] sayilar2 = new int[3];
+            //Array.Copy(Sayilar, sayilar2, 3);
+
+            //////index of arama yapar bulamazsa -1 geri döndürür.
+            //Console.WriteLine(Array.IndexOf(Sayilar,25));
+
+            //reverse dizii ters çevirir
+            //Array.Reverse(Sayilar);
+
+            /////sort dizi elemanlarını sıralar
+            //Array.Sort(Sayilar);
+            #endregion
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
